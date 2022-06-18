adf71693ff6fc5e471e017bdd526e88a7b2491fa
karloresetar <karlo.resetar@gmail.com>
2022-06-18 19:08:36 +0200

    Entiteti za ordere i dodana "poslovna" pravila

--- TDKRSports.CoreBusiness/Services/OrderService.cs
+++ TDKRSports.CoreBusiness/Services/OrderService.cs
@@ -5,7 +5,7 @@ using TDKRSports.CoreBusiness.Models;
 
 namespace TDKRSports.CoreBusiness.Services
 {
-    public class OrderService
+    public class OrderService : IOrderService
     {
 
         public bool ValidateCustomerInformation(
@@ -20,8 +20,8 @@ namespace TDKRSports.CoreBusiness.Services
                 string.IsNullOrWhiteSpace(city) ||
                 string.IsNullOrWhiteSpace(provice) ||
                 string.IsNullOrWhiteSpace(country)) return false;
-            
-            return true;                    
+
+            return true;
         }
 
         public bool ValidateCreateOrder(Order order)
@@ -47,6 +47,51 @@ namespace TDKRSports.CoreBusiness.Services
                     order.CustomerCity,
                     order.CustomerStateProvince,
                     order.CustomerCountry)) return false;
+
+            return true;
+        }
+
+        public bool ValidateUpdateOrder(Order order)
+        {
+            //order has to existi
+            if (order == null) return false;
+            if (!order.OrderId.HasValue) return false;
+
+            //order has to have order line items
+            if (order.LineItems == null || order.LineItems.Count <= 0) return false;
+
+            //Placed Date has to be populated
+            if (!order.DatePlaced.HasValue) return false;
+
+            //Other dates
+            if (order.DateProcessed.HasValue || order.DateProcessing.HasValue) return false;
+
+            //valiate uniqueId
+            if (string.IsNullOrWhiteSpace(order.UniqueId)) return false;
+
+            //validating line items
+            foreach (var item in order.LineItems)
+            {
+                if (item.ProductId <= 0 ||
+                    item.Price < 0 ||
+                    item.Quantity <= 0 ||
+                    item.OrderId == order.OrderId) return false;
+            }
+
+            //validate customer info
+            if (!ValidateCustomerInformation(order.CustomerName,
+                    order.CustomerAddress,
+                    order.CustomerCity,
+                    order.CustomerStateProvince,
+                    order.CustomerCountry)) return false;
+
+            return true;
+        }
+
+        public bool ValidateProcessOrder(Order order)
+        {
+            if (!order.DateProcessed.HasValue ||
+                string.IsNullOrWhiteSpace(order.AdminUser)) return false;
 
             return true;
         }
---
+++ TDKRSports.CoreBusiness/Services/interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using TDKRSports.CoreBusiness.Models;
+
+namespace TDKRSports.CoreBusiness.Services
+{
+    public interface IOrderService
+    {
+        bool ValidateCreateOrder(Order order);
+        bool ValidateCustomerInformation(string name, string address, string city, string provice, string country);
+        bool ValidateProcessOrder(Order order);
+        bool ValidateUpdateOrder(Order order);
+    }
+
+    
+}
