a6e8d6432287c01935e0528909d18b2b5860b295
Ivana Čavka <92532080+ivana-cavka@users.noreply.github.com>
2022-06-24 02:39:21 +0200

    authorization and authentication

---
+++ TDKRSports.Web.Modules/TDKRSports.Web.Common/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace TDKRSports.Web.Common.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ TDKRSports.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TDKRSports.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Route("/authenticate")]
+        public async Task<IActionResult> Authenticate([FromQuery]string user, [FromQuery]string pwd)
+        {
+            if ( user == "admin" && pwd == "adminadmin")
+            {
+                var userClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user),
+                    new Claim(ClaimTypes.Email, "admin@admin.com"),
+                };
+                var userIdentity = new ClaimsIdentity(userClaims, "TDKRSports.CookieAuth");
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync("TDKRSports.CookieAuth", userPrincipal);
+            }
+            return Redirect("/outstandingorders");
+        }
+        [Route("/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/outstandingorders");
+        }
+    }
+}
