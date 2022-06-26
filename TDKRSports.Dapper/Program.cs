3cb57a6dce422e867e7f9b1f445942249f7423e5
Ivana Čavka <92532080+ivana-cavka@users.noreply.github.com>
2022-06-26 19:54:32 +0200

    dapper init

---
+++ TDKRSports.Dapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDKRSports.Dapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TDKR_sport_db;Integrated Security=true";
+            using (IDbConnection conn = new SqlConnection(connectionString))
+            {
+                var result = conn.Query("SELECT * FROM Products");
+                foreach(var record in result)
+                {
+                    Console.Write(record.Name);
+                }
+            }
+        }
+    }
+}
